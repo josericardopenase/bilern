015763031dd30bb3b84f08eca55ab0ff5c40c636
Pepe <pepe@MacBook-Pro-de-Pepe.local>
2024-08-26 10:10:46 +0100

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://arquimedes-website.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind()],
   redirects: {
     "/docs" : "/docs/get-started"
