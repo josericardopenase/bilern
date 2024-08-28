70a3c57caae8243445f37ecc94e5480216344cad
Pepe <pepe@MacBook-Pro-de-Pepe.local>
2024-08-28 23:18:43 +0100

    finished contributors page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	darkMode: 'media',
+	darkMode: 'class',
 	plugins: [
 		require('@tailwindcss/typography'),
 	],
